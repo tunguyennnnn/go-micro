90fa9090796040e63e23b5965d64936fd08cdf14
tunguyennnnn <conghoa522@gmail.com>
2018-08-07 00:05:52 -0400

    simple rpc go

---
+++ rpc/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net"
+  "net/rpc"
+  "github.com/building-microservices-with-go/chapter1/rpc/contract"
+)
+
+const port = 1234
+
+func main() {
+  log.Printf("Server starting on port %v\n", port)
+  StartServer()
+}
+
+type HelloWorldHandler struct{}
+
+func StartServer() {
+  helloWorld := &HelloWorldHandler{}
+  rpc.Register(helloWorld)
+  l, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+  if err != nil {
+    log.Fatal(fmt.Sprintf("Unable to listen on given port: %s", err))
+  }
+  defer l.Close()
+  for {
+    conn, _ := l.Accept()
+    go rpc.ServerConn(conn)
+  }
+}
+
+type HelloWorldHandler struct{}
+
+func (h *HelloWorldHandler) HelloWorld(args *contract.HelloWorldRequest, reply *contract.HelloWorldResponse) error {
+	reply.Message = "Hello " + args.Name
+	return nil
+}
